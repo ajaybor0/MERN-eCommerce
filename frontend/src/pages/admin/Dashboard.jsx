38acebcd392ff3f054d0b87c2b1c54bd9f65fb4a
Ajay Boro <ajayboro7277@gmail.com>
2024-01-16 12:13:09 +0530

    add improve admin dashboard

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
-import { FaStore, FaUsers, FaShoppingBag, FaUsersCog } from 'react-icons/fa';
+import {
+  FaStore,
+  FaUsers,
+  FaShoppingBag,
+  FaUsersCog,
+  FaWallet
+} from 'react-icons/fa';
 import { useGetProductsQuery } from '../../slices/productsApiSlice';
 import { useGetUsersQuery, useAdminsQuery } from '../../slices/usersApiSlice';
 import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
 import Loader from '../../components/Loader';
 import Meta from '../../components/Meta';
-
+import { addCurrency } from '../../utils/addCurrency';
 const Dashboard = () => {
   const { data, isLoading } = useGetProductsQuery({});
   const { data: users, isLoading: isUsersLoading } = useGetUsersQuery({});
   const { data: orders, isLoading: isOrdersLoading } = useGetOrdersQuery({});
+  console.log(orders);
   const { data: admins, isLoading: isAdminsLoading } = useAdminsQuery({});
 
   return (
@@ -20,18 +27,16 @@ const Dashboard = () => {
         {isLoading ? (
           <Loader />
         ) : (
-          <Card className='my-3 p-3 rounded bg-info '>
-            <Row className='align-items-center justify-content-center text-center'>
-              <Col>
-                <Card.Body>
-                  <Card.Title>Products</Card.Title>
-                  <Card.Title>
-                    <strong>{data?.total}</strong>
-                  </Card.Title>
-                </Card.Body>
-              </Col>
-              <Col>
+          <Card className='my-3 p-3 rounded bg-info text-light'>
+            <Row className='align-items-center '>
+              <Col xs={4}>
                 <FaStore size={40} />
+              </Col>
+              <Col xs={8}>
+                <Card.Title>Products</Card.Title>
+                <Card.Title>
+                  <strong>{data?.total}</strong>
+                </Card.Title>
               </Col>
             </Row>
           </Card>
@@ -41,18 +46,16 @@ const Dashboard = () => {
         {isUsersLoading ? (
           <Loader />
         ) : (
-          <Card className='my-3 p-3 rounded bg-info '>
-            <Row className='align-items-center justify-content-center text-center'>
-              <Col>
-                <Card.Body>
-                  <Card.Title>Users</Card.Title>
-                  <Card.Title>
-                    <strong>{users?.length}</strong>
-                  </Card.Title>
-                </Card.Body>
-              </Col>
-              <Col>
+          <Card className='my-3 p-3 rounded bg-danger text-light'>
+            <Row className='align-items-center '>
+              <Col xs={4}>
                 <FaUsers size={40} />
+              </Col>
+              <Col xs={8}>
+                <Card.Title>Users</Card.Title>
+                <Card.Title>
+                  <strong>{users?.length}</strong>
+                </Card.Title>
               </Col>
             </Row>
           </Card>
@@ -62,18 +65,16 @@ const Dashboard = () => {
         {isOrdersLoading ? (
           <Loader />
         ) : (
-          <Card className='my-3 p-3 rounded bg-info '>
-            <Row className='align-items-center justify-content-center text-center'>
-              <Col>
-                <Card.Body>
-                  <Card.Title>Orders</Card.Title>
-                  <Card.Title>
-                    <strong>{orders?.length}</strong>
-                  </Card.Title>
-                </Card.Body>
-              </Col>
-              <Col>
+          <Card className='my-3 p-3 rounded bg-warning text-light'>
+            <Row className='align-items-center '>
+              <Col xs={4}>
                 <FaShoppingBag size={40} />
+              </Col>
+              <Col xs={8}>
+                <Card.Title>Orders</Card.Title>
+                <Card.Title>
+                  <strong>{orders?.length}</strong>
+                </Card.Title>
               </Col>
             </Row>
           </Card>
@@ -83,18 +84,20 @@ const Dashboard = () => {
         {isAdminsLoading ? (
           <Loader />
         ) : (
-          <Card className='my-3 p-3 rounded bg-info '>
-            <Row className='align-items-center justify-content-center text-center'>
-              <Col>
-                <Card.Body>
-                  <Card.Title>Admins</Card.Title>
-                  <Card.Title>
-                    <strong>{admins?.length}</strong>
-                  </Card.Title>
-                </Card.Body>
+          <Card className='my-3 p-3 rounded bg-success text-light '>
+            <Row className='align-items-center '>
+              <Col xs={4}>
+                <FaWallet size={40} />
               </Col>
-              <Col>
-                <FaUsersCog size={40} />
+              <Col xs={8}>
+                <Card.Title>Revenue</Card.Title>
+                <Card.Title>
+                  <strong>
+                    {addCurrency(
+                      orders?.reduce((acc, item) => acc + item.totalPrice, 0)
+                    )}
+                  </strong>
+                </Card.Title>
               </Col>
             </Row>
           </Card>
