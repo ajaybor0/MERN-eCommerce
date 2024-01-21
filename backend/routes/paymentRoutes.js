d19010de3ec3e09c2171088d65af78155d04cd24
Tharuneshwar V <tharuneshwarv@gmail.com>
2024-01-21 20:46:10 +0530

    Add validation for payment routes

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,13 +2,36 @@ import express from 'express';
 
 import { protect } from '../middleware/authMiddleware.js';
 import { config, order, validate } from '../controllers/paymentController.js';
+import validateRequest from '../middleware/validator.js';
+import {body, check} from 'express-validator';
 
 const router = express.Router();
 
+const validator = {
+    order: [
+        check('body').exists().withMessage('Request body is required')
+    ],
+    validate: [
+        body('razorpay_order_id')
+            .notEmpty()
+            .withMessage('Razorpay order ID is required')
+            .trim(),
+        body('razorpay_payment_id')
+            .notEmpty()
+            .withMessage('Razorpay payment ID is required')
+            .trim(),
+        body('razorpay_signature')
+            .notEmpty()
+            .withMessage('Razorpay signature is required')
+            .trim()
+            .escape()
+    ]
+}
+
 router.get('/razorpay/config', config);
 
-router.post('/razorpay/order', protect, order);
+router.post('/razorpay/order', validator.order, validateRequest, protect, order);
 
-router.post('/razorpay/order/validate', protect, validate);
+router.post('/razorpay/order/validate', validator.validate, validateRequest, protect, validate);
 
 export default router;
