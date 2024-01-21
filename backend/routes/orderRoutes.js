281defbbdafb882337f58aa39d54e112b499f530
Tharuneshwar V <tharuneshwarv@gmail.com>
2024-01-21 20:46:32 +0530

    Add validation for order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-const router = express.Router();
 import { protect, admin } from '../middleware/authMiddleware.js';
 import {
   addOrderItems,
@@ -9,12 +8,55 @@ import {
   updateOrderToDeliver,
   getOrders
 } from '../controllers/orderController.js';
+import validateRequest from '../middleware/validator.js';
+import { param, check } from 'express-validator';
 
-router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+const router = express.Router();
+
+const validator = {
+  getOrderById: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  updateOrderToPaid: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  updateOrderToDeliver: [
+    param('id').notEmpty().withMessage('Id is required').isMongoId().withMessage('Invalid Id Format')
+  ],
+  addOrderItems: [
+    check('cartItems').notEmpty().withMessage('Cart items are required'),
+    check('shippingAddress').notEmpty().withMessage('Shipping address is required'),
+    check('paymentMethod').notEmpty().withMessage('Payment method is required'),
+    check('itemsPrice')
+      .notEmpty()
+      .withMessage('Items price is required')
+      .isNumeric()
+      .withMessage('Items price must be a number'),
+    check('taxPrice')
+      .notEmpty()
+      .withMessage('Tax price is required')
+      .isNumeric()
+      .withMessage('Tax price must be a number'),
+    check('shippingPrice')
+      .notEmpty()
+      .withMessage('Shipping price is required')
+      .isNumeric()
+      .withMessage('Shipping price must be a number'),
+    check('totalPrice')
+      .notEmpty()
+      .withMessage('Total price is required')
+      .isNumeric()
+      .withMessage('Total price must be a number')
+  ]
+}
+
+router.route('/')
+  .post(validator.addOrderItems, validateRequest, protect, addOrderItems)
+  .get(protect, admin, getOrders);
 
 router.get('/my-orders', protect, getMyOrders);
-router.get('/:id', protect, getOrderById);
-router.put('/:id/pay', protect, updateOrderToPaid);
-router.put('/:id/deliver', protect, admin, updateOrderToDeliver);
+router.get('/:id', validator.getOrderById, validateRequest, protect, getOrderById);
+router.put('/:id/pay', validator.updateOrderToPaid, validateRequest, protect, updateOrderToPaid);
+router.put('/:id/deliver', validator.updateOrderToDeliver, validateRequest, protect, admin, updateOrderToDeliver);
 
 export default router;
